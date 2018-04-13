f6b880b16187de0225f661cb7f6ad926737fcead
General Assembly <generalassembly@ool-ad03009a.dyn.optonline.net>
2018-04-13 14:40:52 -0400

    updated swiping

--- src/components/matching/SwipeMatch.js
+++ src/components/matching/SwipeMatch.js
@@ -22,6 +22,7 @@ class Card extends React.Component {
   render() {
     return (
       <View style={[styles.card, {backgroundColor: this.props.backgroundColor}]}>
+        <Image style={styles.thumbnail} source={{uri: this.props.image}} />
         <Text>{this.props.text}</Text>
       </View>
     )
@@ -42,18 +43,31 @@ class NoMoreCards extends Component {
   }
 }
 
+const cards = [
+  {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
+  {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
+  {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
+  {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
+  {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
+  {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
+  {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
+  {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
+  {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
+]
+
+const cards2 = [
+  {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
+  {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
+  {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
+  {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
+]
+
 export default class SwipeMatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cards: [
-        {text: 'Tomato', backgroundColor: 'red'},
-        {text: 'Aubergine', backgroundColor: 'purple'},
-        {text: 'Courgette', backgroundColor: 'green'},
-        {text: 'Blueberry', backgroundColor: 'blue'},
-        {text: 'Umm...', backgroundColor: 'cyan'},
-        {text: 'orange', backgroundColor: 'orange'},
-      ]
+      cards: cards,
+      oufOfCards: false
     };
     this.renderMatchScreen = this.renderMatchScreen.bind(this)
     this.renderSettingsScreen = this.renderSettingsScreen.bind(this)
@@ -93,22 +107,44 @@ export default class SwipeMatch extends React.Component {
   handleNope (card) {
     console.log(`Nope for ${card.text}`)
   }
-  handleMaybe (card) {
-    console.log(`Maybe for ${card.text}`)
+
+  cardRemoved (index) {
+    console.log(`The index is ${index}`);
+
+    let CARD_REFRESH_LIMIT = 3
+
+    if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
+      console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
+
+      if (!this.state.outOfCards) {
+        console.log(`Adding ${cards2.length} more cards`)
+
+        this.setState({
+          cards: this.state.cards.concat(cards2),
+          outOfCards: true
+        })
+      }
+
+    }
+
   }
+
   render() {
     // If you want a stack of cards instead of one-per-one view, activate stack mode
     // stack={true}
     return (
       <SwipeCards
         cards={this.state.cards}
+        loop={false}
+
         renderCard={(cardData) => <Card {...cardData} />}
         renderNoMoreCards={() => <NoMoreCards />}
+        showYup={true}
+        showNope={true}
 
         handleYup={this.handleYup}
         handleNope={this.handleNope}
-        handleMaybe={this.handleMaybe}
-        hasMaybeAction
+        cardRemoved={this.cardRemoved.bind(this)}
       />
     )
   }
@@ -116,12 +152,26 @@ export default class SwipeMatch extends React.Component {
 
 const styles = StyleSheet.create({
   card: {
+    alignItems: 'center',
+    borderRadius: 5,
+    overflow: 'hidden',
+    borderColor: 'grey',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    elevation: 1,
+  },
+  noMoreCardsText: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  thumbnail: {
     width: 300,
     height: 300,
   },
-  noMoreCardsText: {
-    fontSize: 22,
-  }
+  text: {
+    fontSize: 20,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
 })
